8c67c9fe276711038f119022daa5a6e95cb867b4
Keshav <kesukrishna@Keshavs-MacBook-Air.local>
2026-06-30 19:13:12 +0530

    Day_24 assignment uploaded

---
+++ Day_24/Q95.c
@@ -0,0 +1,37 @@
+// Write a program to Find longest word. 
+#include <stdio.h>
+
+int main() 
+{
+    char str[200], longest[100];
+    int i = 0, j = 0, maxLen = 0, len = 0;
+    printf("Enter a sentence: ");
+    fgets(str, sizeof(str), stdin);
+    while (1) 
+    {
+        if (str[i] != ' ' && str[i] != '\0' && str[i] != '\n') 
+        {
+            len++;
+        } else 
+        {
+            if (len > maxLen) 
+            {
+                maxLen = len;
+                for (j = 0; j < len; j++) 
+                {
+                    longest[j] = str[i - len + j];
+                }
+                longest[len] = '\0';
+            }
+            len = 0;
+        }
+
+        if (str[i] == '\0')
+            break;
+
+        i++;
+    }
+    printf("Longest word = %s\n", longest);
+    printf("Length = %d", maxLen);
+    return 0;
+}
