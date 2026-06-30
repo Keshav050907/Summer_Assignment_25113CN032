289ab8d710445d62038468e6d72e653c96be5df0
Keshav <kesukrishna@Keshavs-MacBook-Air.local>
2026-06-30 15:07:34 +0530

    Day_22 assignment uploaded

---
+++ Day_22/Q85.c
@@ -0,0 +1,27 @@
+// Write a program to Check palindrome string. 
+#include <stdio.h>
+
+int main() 
+{
+    char str[100];
+    int i, length = 0, palindrome = 1;
+    printf("Enter a string: ");
+    scanf("%s", str);
+    while(str[length] != '\0') 
+    {
+        length++;
+    }
+    for(i = 0; i < length / 2; i++) 
+    {
+        if(str[i] != str[length - i - 1]) 
+        {
+            palindrome = 0;
+            break;
+        }
+    }
+    if(palindrome)
+        printf("String is Palindrome.");
+    else
+        printf("String is Not Palindrome.");
+    return 0;
+}
---
+++ Day_22/Q86.c
@@ -0,0 +1,20 @@
+// Write a program to Count words in a sentence.
+#include <stdio.h>
+
+int main() 
+{
+    char str[200];
+    int i = 0, words = 1;
+    printf("Enter a sentence: ");
+    fgets(str, sizeof(str), stdin);
+    while(str[i] != '\0') 
+    {
+        if(str[i] == ' ') 
+        {
+            words++;
+        }
+        i++;
+    }
+    printf("Number of words = %d", words);
+    return 0;
+}
